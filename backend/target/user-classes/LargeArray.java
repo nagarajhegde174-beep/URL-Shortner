101cc22e80be28c0c21709e99dd0f4d5ad1f6877
nagaraj <nagarajhegde174@gmail.com>
2026-08-08 22:20:00 +0530

    ignore the target files

--- backend/target/user-classes/LargeArray.java
+++
@@ -1,9 +0,0 @@
-public class LargeArray {
-    public static void main(String[] args) {
-        int[] arr = new int[100];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i;
-        }
-        System.out.println("Large array initialized: " + arr.length);
-    }
-}
--- backend/target/user-classes/SingleElement.java
+++
@@ -1,7 +0,0 @@
-public class SingleElement {
-    public static void main(String[] args) {
-        int[] arr = {42};
-        int max = arr[0];
-        System.out.println("Max: " + max);
-    }
-}
